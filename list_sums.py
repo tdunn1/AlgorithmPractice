ca7cd85a2a57ccf6c963979b6fc81d1bdac12d26
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-02-08 10:45:06 -0500

    Update list_sums.py

--- list_sums.py
+++ list_sums.py
@@ -4,13 +4,11 @@ Created on Thu Jan 19 15:09:01 2023
 
 @author: Tim
 """
-
-def array_sum(nums):
+#Returns running sum of an array.
+def array_sum(nums:list):
     l = len(nums)
     s = [0]*l
     s[0] = nums[0]
     for i in range(1, l):
         s[i] = s[i-1] + nums[i]
     return s
-nums = [1,2,7,0,4]
-print(nums, '\n',array_sum(nums))
