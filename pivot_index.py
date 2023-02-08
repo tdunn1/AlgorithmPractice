e84764c82d270798dcc03520fe979c73d2cc8e24
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-02-08 10:46:51 -0500

    Update pivot_index.py

--- pivot_index.py
+++ pivot_index.py
@@ -4,8 +4,9 @@ Created on Thu Jan 19 15:26:27 2023
 
 @author: Tim
 """
-# import pdb
-def pivot_index(nums):
+
+#Find pivot index of array
+def pivot_index(nums:list):
     S = sum(nums)
     left_sum = 0
     for i,x in enumerate(nums):
@@ -13,6 +14,4 @@ def pivot_index(nums):
             return i
         left_sum += x
     return -1
-# pdb.set_trace()
-nums = [1,7,3,6,5,6]
-print(pivot_index(nums))
+
