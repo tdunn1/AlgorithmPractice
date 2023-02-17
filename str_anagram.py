fcd5238afb3d4f4e4887141c95980a9d5e38eec2
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-02-16 19:34:48 -0500

    Add files via upload

---
+++ str_anagram.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 17:04:55 2023
+
+@author: Tim
+"""
+
+from collections import Counter
+
+#Returns start indices of where word is an anagram of s.
+def is_anagram(chunk:str, word:str):
+    return Counter(chunk) == Counter(word)
+
+def anagram_indices(s:str, word:str):
+    results = []
+    for i in range(len(s) - len(word) + 1):
+        chunk = s[i:i+len(word)]
+        if is_anagram(chunk, word):
+            results.append(i)
+    
+    return results
+
+s = 'abxaba'
+word = 'ab'
+
+print(anagram_indices(s,word))
