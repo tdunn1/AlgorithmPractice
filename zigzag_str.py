8d685b577e2a62035216575c44986e238b431f3b
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-03-07 23:00:40 -0500

    Update zigzag_str.py

--- zigzag_str.py
+++ zigzag_str.py
@@ -5,16 +5,31 @@ Created on Fri Mar  3 11:58:37 2023
 @author: Tim
 """
 
-import pdb
 def spaces_num(k:int, row:int, desc:bool) -> int:
     row_ind = row + 1
-    max_spaces = 2*(k-1) - 1
     if desc:
-        spaces = max_spaces - 2*(row_ind)
+        spaces = 2*(k-row_ind) - 1
     else:
-        spaces = max_spaces - 2*(k - row)
+        max_spaces = 2*(k-1) - 1
+        spaces = max_spaces - 2*(k - row_ind)
     return spaces
 
 def is_desc(k, i:int) -> bool:
-    index = i + 1
-    return index % (2*k) < k
+    index = i 
+    return index % (2*(k) - 2) < k-1
+
+def zig_zag(sentence:str, k):
+    sent_len = len(sentence)
+        
+    for row in range(k):
+        i = row
+        line = [' ' for _ in range(sent_len)]
+        
+        while i < sent_len:
+            line[i] = sentence[i]
+            desc = is_desc(k, i)
+            spaces = spaces_num(k,row, desc)
+            i += spaces + 1
+            
+        print(''.join(line))
+    
