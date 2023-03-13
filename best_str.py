8c13e19c4cf037b1f7c6f969c57a5c3e8fdf8717
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-03-13 18:20:14 -0400

    Add files via upload

---
+++ best_str.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 17:51:34 2023
+
+@author: Tim
+"""
+
+def get_best_str(s:str, k):
+    string = list(s)
+   
+    
+    if k == 1:
+        best = string
+        for i in range(1,len(string)):
+            if string[i:] + string[:i] < best:
+                best = string[i:] + string[:i]
+        return ''.join(best)
+    
+    else:
+        return ''.join(sorted(string))
+    
+print(get_best_str('daily',2))
+
+        
---
+++ bubble_swap.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 17:36:42 2023
+
+@author: Tim
+"""
+
+def bubble_swap(s:str, i:int, j:int):
+    string = list(s)
+    
+    while i>0:
+        string = string[1:] + string[:1]
+        i -= 1
+        
+    #move first two characters reversed at end of string    
+    string = string[:1] + string[2:] + string[1:2]
+    string = string[1:] + string[:1]
+    
+    #to original positions
+    while len(string) > j + 1:
+        string = string[1:] + string[:1]
+        j += 1
+        
+    return ''.join(string)
+
+s = 'daily'
+print(bubble_swap(s,5,0))
